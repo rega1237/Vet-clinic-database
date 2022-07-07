14a3e3b1b2ebbe2ae7a57df85e678d6e8d80562e
Rafael Guzman <rega1237@gmail.com>
2022-07-06 22:17:39 -0400

    Create table owners, species, Delete column species, add column species_id and owner_id

--- schema.sql
+++ schema.sql
@@ -13,3 +13,32 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species VARCHAR;
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR,
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
