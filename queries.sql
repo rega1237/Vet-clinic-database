d6bcc17293d218be5dadb8249daf8e12c13a45fb
Rafael Guzman <rega1237@gmail.com>
2022-07-07 22:54:09 -0400

    Make the queries

--- queries.sql
+++ queries.sql
@@ -72,3 +72,85 @@ INNER JOIN owners
     ON animals.owner_id = owners.id
 GROUP BY full_name
 ORDER BY COUNT(owner_id) DESC;
+
+SELECT vets.name, animals.name, visit_date
+FROM vets
+INNER JOIN visits
+    ON vets.id = vet_id
+INNER JOIN animals
+    ON animals.id = animal_id
+WHERE vets.id = 1
+ORDER BY visit_date DESC LIMIT 1;
+
+SELECT vets.name, COUNT(*) AS animals_see
+FROM vets
+INNER JOIN visits
+    ON vets.id = vet_id
+WHERE vets.id = 3
+GROUP BY vets.name;
+
+SELECT vets.name, species.name
+FROM vets
+LEFT JOIN specializations
+    ON vets.id = specializations.vet_id
+FULL JOIN species
+    ON species.id = specializations.species_id;
+
+SELECT vets.name, animals.name, visits.visit_date
+FROM vets
+INNER JOIN visits
+    ON vets.id = visits.vet_id
+INNER JOIN animals
+    ON animals.id = visits.animal_id
+WHERE vets.id = 3 AND visits.visit_date BETWEEN '2020-04-1' AND '2020-08-30';
+
+SELECT animals.name, COUNT(visits.animal_id) AS number_visits
+FROM animals
+INNER JOIN visits
+    ON animals.id = visits.animal_id
+GROUP BY animals.name
+ORDER BY COUNT(visits.animal_id) DESC LIMIT 1;
+
+SELECT vets.name, animals.name, visits.visit_date AS date
+FROM vets
+INNER JOIN visits
+    ON vets.id = visits.vet_id
+INNER JOIN animals
+    ON animals.id = visits.animal_id
+WHERE vets.id = 2
+ORDER BY date LIMIT 1; 
+
+SELECT vets.name, animals.name, visits.visit_date
+FROM vets
+INNER JOIN visits
+    ON vets.id = visits.vet_id
+INNER JOIN animals
+    ON animals.id = visits.animal_id
+ORDER BY visits.visit_date DESC LIMIT 1;
+
+SELECT COUNT(*)
+FROM visits
+INNER JOIN vets
+    ON visits.vet_id = vets.id
+INNER JOIN animals
+    ON visits.animal_id = animals.id
+LEFT JOIN specializations
+    ON vets.id = specializations.vet_id
+WHERE vets.id != (
+    SELECT id FROM vets
+    WHERE name = 'Stephanie Mendez'
+)
+AND specializations.species_id != animals.species_id
+OR specializations.species_id IS NULL;
+
+SELECT species.name, COUNT(species.name)
+FROM visits
+INNER JOIN vets
+    ON visits.vet_id = vets.id
+INNER JOIN animals
+    ON visits.animal_id = animals.id
+INNER JOIN species
+    ON animals.species_id = species.id
+WHERE vets.id = 2
+GROUP BY species.name
+ORDER BY COUNT(species.name) DESC LIMIT 1;
